93cefee542af17a92a1f3c330bd02c74030d94f4
Raymond Yeh <ray@geek.sg>
2020-04-22 14:39:16 +0800

    chore: populating env before deploy

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -8,4 +8,10 @@ aws_access_key_id = ${STAGING_AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${STAGING_AWS_SECRET_ACCESS_KEY}
 EOL
 
+cat > .env << EOL
+SHEETS_API_KEY=${SHEETS_API_KEY}
+SHEETS_ID=${SHEETS_ID}
+SHEETS_RANGE=${SHEETS_RANGE}
+EOL
+
 npm run deploy -- --stage stg
