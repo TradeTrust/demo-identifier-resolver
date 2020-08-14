85547d2f42d6cd9e6365d02f6e80010a2830ad5d
yehjxraymond <yehjxraymond@gmail.com>
2020-08-14 14:59:46 +0800

    feat: search endpoint (#4)
    * feat: search endpoint
    
    * fix: remove unused path param from serverless
    
    Co-authored-by: Raymond Yeh <ray@geek.sg>

---
+++ src/functionHandlers/searchProfiles/searchProfiles.ts
@@ -0,0 +1,45 @@
+import { APIGatewayEvent } from "aws-lambda";
+import createHttpError from "http-errors";
+import { restrictedRequestHandler } from "../../middlewares/handlers";
+import { sheetsToJson } from "../../services/sheets";
+import { config } from "../../config";
+import { memoize } from "../../common/utils";
+
+interface Identity {
+  name: string;
+  remarks: string;
+  identifier: string;
+}
+
+interface Identities {
+  [identifier: string]: Identity;
+}
+
+const searchProfiles = async (event: APIGatewayEvent) => {
+  const query = event.queryStringParameters?.q;
+  if (!query) throw new createHttpError.BadRequest("Query string is not defined");
+  if (query.length < 3) throw new createHttpError.BadRequest("Query string needs to be at least 3 characters");
+
+  const lowercaseQuery = query.toLowerCase();
+
+  const identities = await memoize(
+    () =>
+      sheetsToJson<Identities>({
+        id: config.sheetsId,
+        range: config.sheetsRange,
+        keyBy: "identifier"
+      }),
+    "IDENTITIES"
+  );
+
+  return Object.keys(identities)
+    .map(address => identities[address])
+    .filter(
+      identity =>
+        identity.name.toLowerCase().includes(lowercaseQuery) ||
+        identity.remarks.toLowerCase().includes(lowercaseQuery) ||
+        identity.identifier.toLowerCase().includes(lowercaseQuery)
+    );
+};
+
+export const handler = restrictedRequestHandler(searchProfiles);
