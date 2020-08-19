7f14ba54f6be1920a9c8dda44e8c87ddac3ca784
Sim Boon Long <simboonlong@gmail.com>
2020-08-19 13:35:04 +0800

    feat: add cacheControl

--- src/middlewares/handlers/index.ts
+++ src/middlewares/handlers/index.ts
@@ -1,12 +1,12 @@
 import middy from "middy";
-import { cors } from "middy/middlewares";
+import cors from "@middy/http-cors";
 import httpSecurityHeaders from "@middy/http-security-headers";
 import { withBoundary } from "../withBoundary";
 import { withAuth } from "../withAuth";
 
 export const restrictedRequestHandler = (handler: any) =>
   middy(handler)
-    .use(cors())
+    .use(cors({ cacheControl: "max-age=3600, s-maxage=3600, proxy-revalidate" }))
     .use(httpSecurityHeaders())
     .use(withAuth())
     .use(withBoundary());
